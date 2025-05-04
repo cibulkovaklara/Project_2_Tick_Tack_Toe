87da1413190c418cf5c7ba00b851e998606dde25
cibulkovaklara <cibulkovaklara@seznam.cz>
2025-05-04 14:37:04 +0200

    Pojemnování hlavičky, přidání funkce pro zobrazení úvodního textu s pravidly řádek 8-18

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+projekt_2.py: první projekt do Engeto Online Python Akademie
+
+author: Klara Cibulkova
+email: cibulkovaklara@seznam.cz
+"""
+
+def welcome():
+    print("Welcome to Tic Tac Toe")
+    print("=" * 43)
+    print("GAME RULES:")
+    print("Each player can place one mark (or stone)")
+    print("per turn on the 3x3 grid. The WINNER is")
+    print("who succeeds in placing three of their")
+    print("marks in a:\n* horizontal,\n* vertical or\n* diagonal row")
+    print("=" * 43)
+    print("Let's start the game")
+    print("-" * 43)
+
+    
